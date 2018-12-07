5bdc54b613606815cd491c1a46aebafafcb795a8
Shih Wei <loneswei@gmail.com>
2018-12-07 11:16:32 +0800

    Finished Customer Order
    +Now Customer walk at diff speed towards counter
    +They also order food 1 by 1
    +Reach first order first

--- Astra Pro/Assets/Scenes/Tutorial/Scripts/CCustomer.cs
+++ Astra Pro/Assets/Scenes/Tutorial/Scripts/CCustomer.cs
@@ -3,12 +3,15 @@
 public class CCustomer : MonoBehaviour
 {
     [Tooltip("Movement Speed of Customer")] public float m_fMovementSpeed = 10f;
+
     [HideInInspector] public Vector3 m_QueuePosition;
     [HideInInspector] public int m_iCustomerCount;
-
-    private float m_fCustomerSize;
+    [HideInInspector] public bool m_bReachedTarget = false;
+    [HideInInspector] public bool m_bOrderedFood = false;
+    
     private Vector3 m_Dir;
     private Vector3 m_TargetPosition;
+    private float m_fCustomerSize;
 
 	private void Start ()
     {
@@ -22,24 +25,35 @@ public class CCustomer : MonoBehaviour
     {
         if(m_iCustomerCount == 1)
         {
-            m_Dir = (m_QueuePosition - transform.position).normalized;
-
             m_TargetPosition = m_QueuePosition;
+            m_Dir = (m_QueuePosition - transform.position).normalized;
         }
         else
         {
+            // Later customer walk slower
+            m_fMovementSpeed = m_fMovementSpeed - (4f * (m_iCustomerCount - 1));
+
+            // Later customer stand behind the earlier customer/s
             Vector3 NewQueuePosition = new Vector3(m_QueuePosition.x - (m_fCustomerSize * (m_iCustomerCount - 1)), m_QueuePosition.y, m_QueuePosition.z);
 
             m_TargetPosition = NewQueuePosition;
-
             m_Dir = (NewQueuePosition - transform.position).normalized;
         }
     }
 
-    // Move towards the Target position
-	void Update ()
+    public void OrderFood()
     {
-        if(Vector3.Distance(transform.position, m_TargetPosition) >= 1f)
+        // Show Food Bubble Image
+        transform.GetChild(0).gameObject.SetActive(true);
+        m_bOrderedFood = true;
+    }
+
+	private void Update ()
+    {
+        // Move towards the Target position
+        if(!m_bReachedTarget && Vector3.Distance(transform.position, m_TargetPosition) >= 1f)
             transform.position += m_Dir * m_fMovementSpeed * Time.deltaTime;
+        else
+            m_bReachedTarget = true;
 	}
 }
--- Astra Pro/Assets/Scenes/Tutorial/Scripts/CCustomerSpawner.cs
+++ Astra Pro/Assets/Scenes/Tutorial/Scripts/CCustomerSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class CCustomerSpawner : MonoBehaviour
 {
@@ -6,11 +7,17 @@ public class CCustomerSpawner : MonoBehaviour
 
     [Tooltip("Spawn Point of Customer")] [SerializeField] private Transform m_SpawnPoint;
     [Tooltip("Prefab of Customer")] [SerializeField] private GameObject m_CustomerPrefab;
+    [Tooltip("Time to wait before customer ordering food.")] [SerializeField] private float m_fOrderTiming = 3f;
 
+    private Queue<CCustomer> m_CustomerQ;
+    private float m_fWaitingTime = 0f;
     private int m_iCustomerCount = 0;
 
-    private void Start ()
+    private void Awake ()
     {
+        // Initialise Queue
+        m_CustomerQ = new Queue<CCustomer>();
+
         // Spawn 3 customers at start of game
         for (int i = 0; i < 3; i++)
         {
@@ -33,6 +40,34 @@ public class CCustomerSpawner : MonoBehaviour
         // Set variables for each new Customer
         obj.GetComponent<CCustomer>().m_iCustomerCount = m_iCustomerCount;
         obj.GetComponent<CCustomer>().m_QueuePosition = m_QueuePoint.position;
+
+        // Customer Queue Up
+        m_CustomerQ.Enqueue(obj.GetComponent<CCustomer>());
     }
 
+    private void Update()
+    {
+        // Customers order food 1 by 1 as they reached counter
+        foreach (CCustomer customer in m_CustomerQ)
+        {
+            if (customer.m_bReachedTarget)
+            {
+                if (!customer.m_bOrderedFood)
+                {
+                    m_fWaitingTime += Time.deltaTime;
+                    
+                    // Wait for a few seconds(Let Customer "Think" before Ordering food)
+                    if (m_fWaitingTime > m_fOrderTiming)
+                    {
+                        customer.OrderFood();
+                        m_fWaitingTime = 0f;
+                        m_CustomerQ.Dequeue();
+                    }
+                    else
+                        break;
+                }
+            }
+        }
+
+    }
 }
