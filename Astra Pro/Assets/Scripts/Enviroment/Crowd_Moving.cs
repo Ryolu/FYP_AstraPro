b726637c5a00116d231d960a5361234838b959b7
Hayashikiyoi <Hayashikiyoi@gmail.com>
2018-12-07 14:56:09 +0800

    Adding Day and Night

---
+++ Astra Pro/Assets/Scripts/Enviroment/Crowd_Moving.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowd_Moving : MonoBehaviour {
+    public float movingSpeed = 1.0f;
+
+    private void OnEnable()
+    {
+        Invoke("Destroy",10f);    
+    }
+    private void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+       transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
+	}
+}
---
+++ Astra Pro/Assets/Scripts/Enviroment/Crowd_Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowd_Spawner : MonoBehaviour {
+    //GameObject Spawner
+    public GameObject People_1;
+    public GameObject C_Spawner;
+    //Time of Spawning objects
+    float ElapseTime;
+    public float EndTime = 1;
+
+    private void onSpawnCrowd()
+    {
+        GameObject Spawner = CObjectPool.m_sInstance.GetPooledObject(People_1);
+
+        Spawner.transform.position = C_Spawner.transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        ElapseTime += Time.deltaTime;
+        if (ElapseTime > EndTime)
+        {
+            onSpawnCrowd();
+            ElapseTime = 0;
+        }
+	}
+}
