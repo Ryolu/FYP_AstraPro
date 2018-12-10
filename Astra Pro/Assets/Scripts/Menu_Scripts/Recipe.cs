0f229eb058f2325c9db3cda8eff9d6e873c03990
Hayashikiyoi <Hayashikiyoi@gmail.com>
2018-12-10 10:41:24 +0800

    suibian

---
+++ Astra Pro/Assets/Scripts/Menu_Scripts/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe : MonoBehaviour {
+
+    public bool b_Completed = false;
+
+    public bool[] otah;
+    public bool[] Fish_Head_Curry;
+    public bool[] Mee_Siam;
+    public bool[] Laksa;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
