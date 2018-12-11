472c00b52e363fb9e2cff00fa5c67012502a0000
Jasper <rendescar63@gmail.com>
2018-12-11 14:09:52 +0800

    Revert "Revert "New foods and ingredients""
    This reverts commit f4f9afc48e3fdda676603d005e2d434cf89a1e5a.

--- Astra Pro/Assets/Scripts/Menu_Scripts/Audio_Manager.cs
+++ Astra Pro/Assets/Scripts/Menu_Scripts/Audio_Manager.cs
@@ -2,44 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Audio;
-
 public class Audio_Manager : MonoBehaviour {
-    
-    public Slider BGM, SFX;
-    public AudioMixer masterMixer;
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(this);
-    }
+    public Slider BGM, SFX;
+
     public void BGMMaxAudio(float Volume)
     {
         BGM.value = Volume;
-        masterMixer.SetFloat("BackgroundMusic", 0);
     }
     public void BGMMinAudio()
     {
-        BGM.value = -80;
-        masterMixer.SetFloat("BackgroundMusic", -80);
+        BGM.value = 0;
     }
     public void SFXMaxAudio(float Volume)
     {
         SFX.value = Volume;
-        masterMixer.SetFloat("SoundEffects", 0);
     }
     public void SFXMinAudio()
     {
-        SFX.value = -80;
-        masterMixer.SetFloat("SoundEffects", -80);
-    }
-
-    public void SetSfxLvl(float sfxLvl)
-    {
-        masterMixer.SetFloat("SoundEffects", sfxLvl);
-    }
-    public void SetBgmLvl(float bgmLvl)
-    {
-        masterMixer.SetFloat("BackgroundMusic", bgmLvl);
+        SFX.value = 0;
     }
 }
