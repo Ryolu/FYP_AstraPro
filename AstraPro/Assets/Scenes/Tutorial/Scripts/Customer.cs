98949d910b39555fe36db50ee27cfd413033d238
Shih Wei <loneswei@gmail.com>
2018-12-17 12:24:48 +0800

    Added Timer
    +Customer will "Leave" when Timer depletes to empty

--- AstraPro/Assets/Scenes/Tutorial/Scripts/Customer.cs
+++ AstraPro/Assets/Scenes/Tutorial/Scripts/Customer.cs
@@ -1,25 +1,60 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Customer : MonoBehaviour
 {
-    [Tooltip("Movement Speed of Customer")] public float movementSpeed = 25f;
-    [Tooltip("Movement Speed of Customer")] public float rotateSpeed = 80f;
+    [Tooltip("Movement Speed of Customer")] [SerializeField] private float movementSpeed = 25f;
+    [Tooltip("Rotate Speed of Customer")] [SerializeField] private float rotateSpeed = 80f;
+    [Tooltip("How long the Customer will wait for food")] [SerializeField] private float waitTiming = 30f;
+    [Tooltip("Timer Filler Image")] [SerializeField] private Image timerImage;
 
     [HideInInspector] public Vector3 queuePosition;
     [HideInInspector] public int customerId;
     [HideInInspector] public bool reachedTarget = false;
     [HideInInspector] public bool orderedFood = false;
-    
+
     private Vector3 dir;
     private Vector3 targetPosition;
     private float customerSizeX;
+    private Gradient greenYellowGradient;
+    private Gradient yellowRedGradient;
 
-	private void Start ()
+    private void Start ()
     {
         customerSizeX = transform.lossyScale.x * 25f;
         CalculateDir();
-	}
+
+        InitiateColor();
+    }
 	
+    // Initiate Gradients, which is used to change color based on fillAmount of timerImage
+    private void InitiateColor()
+    {
+        greenYellowGradient = new Gradient();
+        var ck1 = new GradientColorKey[2];
+        ck1[0].color = Color.green;
+        ck1[0].time = 1f;
+
+        ck1[1].color = Color.yellow;
+        ck1[1].time = 0.5f;
+
+        var ak1 = new GradientAlphaKey[0];
+
+        greenYellowGradient.SetKeys(ck1, ak1);
+
+        yellowRedGradient = new Gradient();
+        var ck2 = new GradientColorKey[2];
+        ck2[0].color = Color.yellow;
+        ck2[0].time = 0.5f;
+
+        ck2[1].color = Color.red;
+        ck2[1].time = 0f;
+
+        var ak2 = new GradientAlphaKey[0];
+
+        yellowRedGradient.SetKeys(ck2, ak2);
+    }
+
     // Calculate Direction for customer to move and Record down the Target position based on number of customer
     public void CalculateDir()
     {
@@ -62,6 +97,29 @@ public class Customer : MonoBehaviour
 
     private void Update ()
     {
+        // Customer Waiting for food
+        if(orderedFood)
+        {
+            // Reduce fillAmount of Timer Filler Image(visual feedback) over waitTiming
+            timerImage.fillAmount -= (1f / waitTiming) * Time.deltaTime;
+
+            // Left more than half the time -> Image turning from green to yellow
+            if(timerImage.fillAmount >= 0.5f)
+            {
+                timerImage.color = greenYellowGradient.Evaluate(timerImage.fillAmount / 1f);
+            }
+            // Left less than half the time -> Image turning from yellow to red
+            else if(timerImage.fillAmount > 0f)
+            {
+                timerImage.color = yellowRedGradient.Evaluate(timerImage.fillAmount / 1f);
+            }
+            else if(timerImage.fillAmount <= 0f)
+            {
+                // Leave
+                CustomerSpawner.Instance.Leave(customerId);
+            }
+        }
+
         // Move towards the Target position
         if(!reachedTarget && Vector3.Distance(transform.position, targetPosition) >= 1f)
         {
