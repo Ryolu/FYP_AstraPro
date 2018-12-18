a75680436bc35f25a092ac9f14f0c900fe3c3c2b
Hayashikiyoi <Hayashikiyoi@gmail.com>
2018-12-18 17:41:45 +0800

    Added in Stuffs

--- AstraPro/Assets/Scripts/Enviroment/Crowd_Moving.cs
+++ AstraPro/Assets/Scripts/Enviroment/Crowd_Moving.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Crowd_Moving : MonoBehaviour {
-    float movingSpeed = 1.0f;
+    public float movingSpeed = 1.0f;
 
     private void OnEnable()
     {
