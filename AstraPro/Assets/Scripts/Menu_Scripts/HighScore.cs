f760c18943158f348e9c17aa648117b5454a0029
Shih Wei <loneswei@gmail.com>
2018-12-19 18:05:34 +0800

    highscore system

--- AstraPro/Assets/Scripts/Menu_Scripts/HighScore.cs
+++ AstraPro/Assets/Scripts/Menu_Scripts/HighScore.cs
@@ -20,16 +20,16 @@ public class HighScore : MonoBehaviour {
 
         ScoreStorage ScoreCheck = JsonUtility.FromJson<ScoreStorage>(jsonString);
 
-        highestScoreText[0].text = ScoreCheck.First.ToString();
-        highestScoreText[1].text = ScoreCheck.Second.ToString();
-        highestScoreText[2].text = ScoreCheck.Third.ToString();
-        highestScoreText[3].text = ScoreCheck.Fourth.ToString();
-        highestScoreText[4].text = ScoreCheck.Fifth.ToString();
-        highestScoreText[5].text = ScoreCheck.Sixth.ToString();
-        highestScoreText[6].text = ScoreCheck.Seventh.ToString();
-        highestScoreText[7].text = ScoreCheck.Eighth.ToString();
-        highestScoreText[8].text = ScoreCheck.Ninth.ToString();
-        highestScoreText[9].text = ScoreCheck.Tenth.ToString();
+        //highestScoreText[0].text = ScoreCheck.First.ToString();
+        //highestScoreText[1].text = ScoreCheck.Second.ToString();
+        //highestScoreText[2].text = ScoreCheck.Third.ToString();
+        //highestScoreText[3].text = ScoreCheck.Fourth.ToString();
+        //highestScoreText[4].text = ScoreCheck.Fifth.ToString();
+        //highestScoreText[5].text = ScoreCheck.Sixth.ToString();
+        //highestScoreText[6].text = ScoreCheck.Seventh.ToString();
+        //highestScoreText[7].text = ScoreCheck.Eighth.ToString();
+        //highestScoreText[8].text = ScoreCheck.Ninth.ToString();
+        //highestScoreText[9].text = ScoreCheck.Tenth.ToString();
     }
 
     public void OnHighScore()
--- AstraPro/Assets/Scripts/Menu_Scripts/highScoreControls.cs
+++ AstraPro/Assets/Scripts/Menu_Scripts/highScoreControls.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class highScoreControls : MonoBehaviour
@@ -12,85 +13,81 @@ public class highScoreControls : MonoBehaviour
     [SerializeField] Sprite[] spriteCollection;
     [SerializeField] RectTransform content;
     [SerializeField] GameObject imageItemPrefab;
+    
+    [Header("Element Size")]
+    [SerializeField] float elementX;
+    [SerializeField] float elementY;
+    [SerializeField] float focusX;
+    [SerializeField] float focusY;
 
-    [SerializeField] CanvasGroup canvasGroup;
-
-    [Header("Grid")]
-
-    [Range(1, 10)]
-    [SerializeField] int rowsNumber = 3;
-    [Range(1, 10)]
-    [SerializeField] int colsNumber = 4;
-
-    Vector2 pageSize;
-    int numberOfPages = 0;
-
-    Vector2 defaultSize;   
-
-    [Header("Scroll")]
-
-    [Range(0.1f, 10)]
-    [SerializeField] float scrollSpeed = 4f;
-
-    float scrollStep = 0;
-
-    [Header("View")]
-    [SerializeField] RectTransform viewRect;
-
-    Vector2 defaultPosition;
-
-    [Range(0.1f, 16f)]
-    [SerializeField] float animationSpeed = 2;
-
-    ImageItem selectedItem = null;
-
-    bool animated = false;
-    float t = 0;
- 
-    int currentPage = 0;
+    int focus = 0;
+    List<LayoutElement> generatedSprites;
 
     void Start()
     {
-        pageSize = new Vector2(Screen.width, Screen.height);
-        defaultSize = new Vector2(Screen.width / colsNumber, Screen.height / rowsNumber);
+        //pageSize = new Vector2(Screen.width, Screen.height);
+        //defaultSize = new Vector2(Screen.width / colsNumber, Screen.height / rowsNumber);
+        //
+        //Vector2 halfAdd = new Vector2(defaultSize.x / 2, -defaultSize.y / 2);
+        //
+        //int imagesOnPage = rowsNumber * colsNumber;
+        //numberOfPages = (int)Mathf.Ceil((float)spriteCollection.Length / imagesOnPage);
+        //
+        //int imageIndex = 0;
+        //
+        //for (int p = 0; p < numberOfPages; p++)
+        //{
+        //    int imagesOnCurrentPage = Mathf.Min(spriteCollection.Length - p * imagesOnPage, imagesOnPage);
+        //
+        //    for (int i = 0; i < imagesOnCurrentPage; i++)
+        //    {
+        //        GameObject currentItem = Instantiate(imageItemPrefab);
+        //        currentItem.transform.SetParent(content, false);
+        //
+        //        RectTransform currentRect = currentItem.GetComponent<RectTransform>();
+        //        currentRect.sizeDelta = defaultSize;
+        //
+        //        float X = pageSize.x * p + defaultSize.x * (i % colsNumber);
+        //        float Y = defaultSize.y * (i / colsNumber);
+        //
+        //        currentRect.anchoredPosition = new Vector2(X, -Y) + halfAdd;
+        //
+        //        Image currentImage = currentItem.GetComponent<Image>();
+        //        currentImage.sprite = spriteCollection[imageIndex];
+        //        imageIndex++;
+        //
+        //        //ImageItem currentImageItem = currentItem.GetComponent<ImageItem>();
+        //        //currentImageItem.OnClick += CurrentImageItem_OnClick;
+        //    }
+        //}
+        //
+        //content.sizeDelta = new Vector2(content.rect.width, -defaultSize.y * 11);
+        ////content.sizeDelta = new Vector2(1000, 1000);
+        //
+        //if (numberOfPages > 1)
+        //    scrollStep = 1f / (numberOfPages - 1);
 
-        Vector2 halfAdd = new Vector2(defaultSize.x / 2, -defaultSize.y / 2);
-
-        int imagesOnPage = rowsNumber * colsNumber;
-        numberOfPages = (int)Mathf.Ceil((float)spriteCollection.Length / imagesOnPage);
-
-        int imageIndex = 0;
-
-        for (int p = 0; p < numberOfPages; p++)
+        generatedSprites = new List<LayoutElement>();
+        
+        for (int i = 0; i < spriteCollection.Length; i++)
         {
-            int imagesOnCurrentPage = Mathf.Min(spriteCollection.Length - p * imagesOnPage, imagesOnPage);
+            GameObject currentItem = Instantiate(imageItemPrefab);
+            currentItem.transform.SetParent(content, false);
 
-            for (int i = 0; i < imagesOnCurrentPage; i++)
-            {
-                GameObject currentItem = Instantiate(imageItemPrefab);
-                currentItem.transform.SetParent(content.transform, false);
+            LayoutElement element = currentItem.GetComponent<LayoutElement>();
+            generatedSprites.Add(element);
+            
+            element.preferredWidth = elementX;
+            element.preferredHeight = elementY;
 
-                RectTransform currentRect = currentItem.GetComponent<RectTransform>();
-                currentRect.sizeDelta = defaultSize;
-
-                float X = pageSize.x * p + defaultSize.x * (i % colsNumber);
-                float Y = defaultSize.y * (i / colsNumber);
-
-                currentRect.anchoredPosition = new Vector2(X, -Y) + halfAdd;
-
-                Image currentImage = currentItem.GetComponent<Image>();
-                currentImage.sprite = spriteCollection[imageIndex];
-                imageIndex++;
-
-                //ImageItem currentImageItem = currentItem.GetComponent<ImageItem>();
-                //currentImageItem.OnClick += CurrentImageItem_OnClick;
-            }
+            Image currentImage = currentItem.GetComponent<Image>();
+            currentImage.sprite = spriteCollection[i];
         }
 
-        content.sizeDelta = new Vector2(Screen.width * numberOfPages, Screen.height);
+        content.sizeDelta = new Vector2(content.rect.width, (elementY + 25) * 10 + 900);
 
-        if (numberOfPages > 1)
-            scrollStep = 1f / (numberOfPages - 1);
+        //ImageItem currentImageItem = currentItem.GetComponent<ImageItem>();
+        //currentImageItem.OnClick += CurrentImageItem_OnClick;
 
         NuitrackManager.onNewGesture += NuitrackManager_onNewGesture;        
     }
@@ -118,61 +115,85 @@ public class highScoreControls : MonoBehaviour
 
     private void Update()
     {
-        switch (currentViewMode)
-        {
-            case ViewMode.View:
+        //scrollRect.verticalScrollbar.value = Mathf.Lerp(scrollRect.verticalScrollbar.value, scrollStep, Time.deltaTime * scrollSpeed);
+        //switch (currentViewMode)
+        //{
+        //    case ViewMode.View:
 
-                if (t < 1)
-                {
-                    t += Time.deltaTime * animationSpeed;
+        //        if (t < 1)
+        //        {
+        //            t += Time.deltaTime * animationSpeed;
 
-                    canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0, t);
+        //            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0, t);
 
-                    selectedItem.image.rectTransform.sizeDelta = Vector2.Lerp(selectedItem.image.rectTransform.sizeDelta, pageSize, t);
-                    selectedItem.transform.localPosition = Vector2.Lerp(selectedItem.transform.localPosition, Vector2.zero, t);
-                }
+        //            selectedItem.image.rectTransform.sizeDelta = Vector2.Lerp(selectedItem.image.rectTransform.sizeDelta, pageSize, t);
+        //            selectedItem.transform.localPosition = Vector2.Lerp(selectedItem.transform.localPosition, Vector2.zero, t);
+        //        }
 
-                break;
+        //        break;
 
-            case ViewMode.Preview:
+        //    case ViewMode.Preview:
 
-                if (animated)
-                {
-                    if (t > 0)
-                    {
-                        t -= Time.deltaTime * animationSpeed;
+        //        if (animated)
+        //        {
+        //            if (t > 0)
+        //            {
+        //                t -= Time.deltaTime * animationSpeed;
 
-                        canvasGroup.alpha = Mathf.Lerp(1, canvasGroup.alpha, t);
+        //                canvasGroup.alpha = Mathf.Lerp(1, canvasGroup.alpha, t);
 
-                        selectedItem.image.rectTransform.sizeDelta = Vector2.Lerp(defaultSize, selectedItem.image.rectTransform.sizeDelta, t);
+        //                selectedItem.image.rectTransform.sizeDelta = Vector2.Lerp(defaultSize, selectedItem.image.rectTransform.sizeDelta, t);
 
-                        selectedItem.transform.localPosition = Vector2.Lerp(defaultPosition, selectedItem.transform.localPosition, t);
-                        selectedItem.transform.localRotation = Quaternion.Lerp(Quaternion.identity, selectedItem.transform.localRotation, t);
-                        selectedItem.transform.localScale = Vector3.Lerp(Vector3.one, selectedItem.transform.localScale, t);
-                    }
-                    else
-                    {
-                        selectedItem.transform.SetParent(content, true);
-                        selectedItem.interactable = true;
-                        canvasGroup.interactable = true;
-                        selectedItem = null;
-                        animated = false;
-                    }
-                }
-                else
-                    scrollRect.horizontalScrollbar.value = Mathf.Lerp(scrollRect.horizontalScrollbar.value, scrollStep * currentPage, Time.deltaTime * scrollSpeed);
+        //                selectedItem.transform.localPosition = Vector2.Lerp(defaultPosition, selectedItem.transform.localPosition, t);
+        //                selectedItem.transform.localRotation = Quaternion.Lerp(Quaternion.identity, selectedItem.transform.localRotation, t);
+        //                selectedItem.transform.localScale = Vector3.Lerp(Vector3.one, selectedItem.transform.localScale, t);
+        //            }
+        //            else
+        //            {
+        //                selectedItem.transform.SetParent(content, true);
+        //                selectedItem.interactable = true;
+        //                canvasGroup.interactable = true;
+        //                selectedItem = null;
+        //                animated = false;
+        //            }
+        //        }
+        //        else
+        //            scrollRect.verticalScrollbar.value = Mathf.Lerp(scrollRect.verticalScrollbar.value, scrollStep * currentPage, Time.deltaTime * scrollSpeed);
 
-                break;
-        }
+        //        break;
+        //}
     }
 
     private void NuitrackManager_onNewGesture(nuitrack.Gesture gesture)
     {
-        if (gesture.Type == nuitrack.GestureType.GestureSwipeUp)
-            currentPage = Mathf.Clamp(++currentPage, 0, numberOfPages);
+        //if (gesture.Type == nuitrack.GestureType.GestureSwipeUp)
+        //    currentPage = Mathf.Clamp(++currentPage, 0, numberOfPages);
+        //
+        //if (gesture.Type == nuitrack.GestureType.GestureSwipeDown)
+        //    currentPage = Mathf.Clamp(--currentPage, 0, numberOfPages);
 
-        if (gesture.Type == nuitrack.GestureType.GestureSwipeDown)
-            currentPage = Mathf.Clamp(--currentPage, 0, numberOfPages);
+    }
 
+    public void ChangeFocus()
+    {
+        focus = Mathf.FloorToInt(( 1 - scrollRect.verticalScrollbar.value ) * 10);
+        Debug.Log(focus);
+
+        if (focus == 10)
+            focus = 9;
+        
+        for (int i = 0; i < generatedSprites.Count; i++)
+        {
+            if (i == focus)
+            {
+                generatedSprites[i].preferredWidth = focusX * 1.25f;
+                generatedSprites[i].preferredHeight = focusY * 1.25f;
+            }
+            else
+            {
+                generatedSprites[i].preferredWidth = elementX;
+                generatedSprites[i].preferredHeight = elementY;
+            }
+        }
     }
 }
