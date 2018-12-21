f0f87693df14b0bb9682f22be4f9cb54745b3db6
Hayashikiyoi <Hayashikiyoi@gmail.com>
2018-12-21 14:15:10 +0800

    Setting light switches

---
+++ AstraPro/Assets/Scripts/Enviroment/SetSunLight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSunLight : MonoBehaviour {
+
+    public Renderer lightwall;
+
+    Material sky;
+
+    public Renderer water;
+
+    public Transform stars;
+    public Transform worldProbe;
+
+	// Use this for initialization
+	void Start () {
+        sky = RenderSettings.skybox;
+	}
+
+    bool lighton = false;
+
+	// Update is called once per frame
+	void Update () {
+        stars.transform.rotation = transform.rotation;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            lighton = !lighton;
+        }
+
+        if (lighton)
+        {
+            Color final = Color.white * Mathf.LinearToGammaSpace(5);
+            lightwall.material.SetColor("EmissionColor", final);
+            DynamicGI.SetEmissive(lightwall, final);
+        }
+        else
+        {
+            Color final = Color.white * Mathf.LinearToGammaSpace(0);
+            lightwall.material.SetColor("EmissionColor", final);
+            DynamicGI.SetEmissive(lightwall, final);
+        }
+
+        Vector3 tvec = Camera.main.transform.position;
+        worldProbe.transform.position = tvec;
+
+        water.material.mainTextureOffset = new Vector2(Time.time / 100.0f, 0);
+        water.material.SetTextureOffset("_DetailAlbedoMap", new Vector2(0, Time.time / 80));
+	}
+}
